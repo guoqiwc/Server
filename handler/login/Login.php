26d68080c1baea70c036518bc5e25d485f6e4a25
guoqiwc <guoqiwc@163.com>
2014-02-28 01:10:31 -0800

    1.修改登陆，登出协议

--- handler/login/Login.php
+++ handler/login/Login.php
@@ -4,20 +4,21 @@
  */
 class Login implements Handler {
 	private $package;
-	function __construct($package) {
-		$this->package = $package;
+	function __construct($bin) {
+		$this->package = new CSLoginRequestMessage ( $bin );
 	}
 	function handle() {
-		// TODO 更改
-		$mac = "FF-FF-FF-FF-FF-FF";
-		$os = "Windows 8.1 X64 企业版";
-		$version = "2.1.4";
-		$login = 1; // 1为登陆 0为退出
 		$ip = $_SERVER ["REMOTE_ADDR"];
-		$time = ( int ) microtime ( true );
+		$mac = $this->package->getMacId ();
+		$os = $this->package->getOSName ();
+		$version = $this->package->getVersion ();
+		$time = $this->package->getTimeStamp ();
+		settype ( $time, "float" );
+		$login = 1; // 1为登陆 0为退出
+		
 		$mysql = Mysql::getInstence ();
 		if ($mysql->insert ( "INSERT INTO `t_login`(user_id,mac,ip,os,version,time,login) VALUES ('1', '$mac', '$ip', '$os', '$version', '$time', '$login');" ) != false) {
-			// TODO 增加成功返回包
+			return;
 		} else {
 		}
 	}
