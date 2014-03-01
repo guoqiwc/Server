952226220d2ea6c4599a8ebd9d8a17b5cb741103
guoqiwc <guoqiwc@163.com>
2014-03-01 00:18:18 -0800

    1.修改协议

--- message/ByteTools.php
+++ message/ByteTools.php
@@ -2,14 +2,78 @@
 /**
  * 协议解析工具
  */
-/*
- * class ByteTools { private $byteArray = ""; private $position = 0; public function __construct($bin = null) { $this->byteArray = ($bin == null ? array () : $bin); } public function setPosition($position) { if (count ( $this->byteArray ) <= $position) { $count = count ( $this->byteArray ); for($i = 0; $i < $position - $count + 1; ++ $i) { $this->writeByte ( 0 ); } } $this->position = $position; } public function writeByte($byte) { $this->byteArray [$this->position] = (0xff & $byte); $this->position += 1; } public function readByte() { $byte = $this->byteArray [$this->position]; $this->position += 1; $byte = unpack ( "H*", $byte ); return intval ( $byte [1], 16 ); } public function writeShort($short) { $this->byteArray [$this->position] = dechex ( (0xff & $short) ); $this->byteArray [$this->position + 1] = dechex ( (0xff00 & $short) >> 8 ); $this->position += 2; } public function readShort() { $str = substr ( $this->byteArray, $this->position, $this->position + 2 ); $short = unpack ( "S", $str ); $this->position += 2; return $short [1]; } public function writeInt($int) { $this->byteArray [$this->position] = (0xFF & $int); $this->byteArray [$this->position + 1] = (0xFF00 & $int) >> 8; $this->byteArray [$this->position + 2] = (0xFF0000 & $int) >> 16; $this->byteArray [$this->position + 3] = (0xFF000000 & $int) >> 24; $this->position += 4; } public function readInt() { $str = substr ( $this->byteArray, $this->position, $this->position + 4 ); $int = unpack ( "i", $str ); $this->position += 4; return $int [1]; } public function writeLongString($string) { $length = strlen ( $string ); $this->writeShort ( $length ); for($index = 0; $index < $length; ++ $index) { $this->byteArray [$this->position ++] = $string [$index]; // >= 128 ? ord ( $string [$index] ) - 256 : ord ( $string [$index] ); } } public function readLongString() { $length = $this->readShort (); $str = ""; for($index = 0; $index < $length; ++ $index) { $str .= ($this->byteArray [$this->position ++]); // >= 128 ? ord ( $string [$index] ) - 256 : ord ( $string [$index] ); } return $str; } public function toString() { $str = ""; for($index = 0; $index < count ( $this->byteArray ); ++ $index) { $str .= $this->byteArray [$index]; } return $str; } public function getByteArray() { $str = ""; for($index = 0; $index < count ( $this->byteArray ); ++ $index) { $str .= $this->byteArray [$index]; } return $str; } }
- */
 class ByteTools {
-	private $byteArray = "";
+	private $byteArray = array ();
 	private $position = 0;
-	function __construct() {
-		
+	function __construct($bin = null) {
+		if ($bin != null) {
+			$this->byteArray = substr ( $bin, 0, strlen ( $bin ) );
+		}
+	}
+	function setPosition($position) {
+		$count = count ( $this->byteArray );
+		if ($count < $position) {
+			for($index = $count; $index < $position; $index ++) {
+				$this->byteArray [] = 0x00;
+			}
+		}
+		$this->position = $position;
+	}
+	function readByte() {
+		$value = substr ( $this->byteArray, $this->position, 1 );
+		$data = unpack ( "c", $value );
+		$this->position += 1;
+		return $data [1];
+	}
+	function readShort() {
+		$value = substr ( $this->byteArray, $this->position, 2 );
+		$data = unpack ( "s", $value );
+		$this->position += 2;
+		return $data [1];
+	}
+	function readInt() {
+		$value = substr ( $this->byteArray, $this->position, 4 );
+		$data = unpack ( "l", $value );
+		$this->position += 4;
+		return $data [1];
+	}
+	function readLongString() {
+		$length = $this->readShort ();
+		$str = "";
+		for($index = 0; $index < $length; ++ $index) {
+			$str .= ($this->byteArray [$this->position ++]);
+		}
+		return $str;
+	}
+	function writeByte($value) {
+		$val = intval ( $value );
+		$this->byteArray [$this->position ++] = ($val & 0xff);
+	}
+	function writeShort($value) {
+		$val = intval ( $value );
+		$this->byteArray [$this->position ++] = ($val & 0xff);
+		$this->byteArray [$this->position ++] = ($val >> 8 & 0xff);
+	}
+	function writeInt($value) {
+		$val = ( int ) $value;
+		$this->byteArray [$this->position ++] = ($val & 0xff);
+		$this->byteArray [$this->position ++] = ($val >> 8 & 0xff);
+		$this->byteArray [$this->position ++] = ($val >> 16 & 0xFF);
+		$this->byteArray [$this->position ++] = ($val >> 24 & 0xff);
+	}
+	function writeLongString($string) {
+		$length = strlen ( $string );
+		$this->writeShort ( $length );
+		for($index = 0; $index < $length; ++ $index) {
+			$this->byteArray [$this->position ++] = ord ( $string [$index] );
+		}
+	}
+	function getByteArray() {
+		$string = "";
+		foreach ( $this->byteArray as $chr ) {
+			$string .= chr ( $chr );
+		}
+		return $string;
 	}
 }
 ?>
