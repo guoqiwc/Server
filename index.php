25ffb964b3b56945cd39f163972492aca999a169
guoqiwc <guoqiwc@163.com>
2014-03-04 02:45:12 -0800

    1. 修改接口位置

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
 require "message/Message.php";
-require "handler/interfaces/Handler.php";
+require "util/Mysql.php";
+
+interface Handler {
+	// 处理函数
+	function handle();
+}
+require "handler/login/Login.php";
+require "handler/login/Logout.php";
+require "handler/error/Error.php";
+require "handler/word/Title.php";
+require "handler/word/LoadingPage.php";
+require "handler/broadcast/Broadcast.php";
+require "handler/behavior/HelpWindow.php";
+require "handler/behavior/AboutWindow.php";
+require "handler/behavior/SettingWindow.php";
+require "handler/behavior/SuspensionWindow.php";
+require "handler/behavior/BehaviorMainWindow.php";
+require "handler/behavior/BehaviorAllWindow.php";
+require "handler/behavior/WebshotWindow.php";
+require "handler/behavior/Webshot.php";
+
 //require "Resources/GetPicture.php";
 $bin = file_get_contents ( "php://input" );
 if ($bin == null || $bin == "") {
