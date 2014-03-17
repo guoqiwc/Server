e8ec69f761106e8e3170c1da74948b845f0d0ea1
guoqiwc <guoqiwc@163.com>
2014-03-17 20:39:15 +0800

    1.更新协议

--- handler/behavior/UserBehavior.php
+++ handler/behavior/UserBehavior.php
@@ -5,6 +5,24 @@ class UserBehavior implements Handler {
 		$this->package = new CSUserBehaviorRequestMessage ( $bin );
 	}
 	function handle() {
+		$ip = $_SERVER ["REMOTE_ADDR"];
+		$mac = $this->package->getMacId ();
+		$os = "";
+		$version = "";
+		$time = $this->package->getLogOffTimeStamp ();
+		$login = 0; // 1为登陆 0为退出
+		$mysql = Mysql::getInstence ();
+		
+		// 1.检查是否为重复提交的数据
+		$sql = "SELECT * FROM t_login WHERE mac = '$mac' AND time = '$time' AND login = 0;";
+		$result = $mysql->query ( $sql );
+		if ($result->num_rows > 0) {
+			// 开始攒包
+			$this->package = new SCUserBehaviorRequestMessage ();
+			$this->package->setIsSuccess ( 1 );
+			echo $this->package->build ()->getByteArray ();
+			return;
+		}
 	}
 }
 
--- index.php
+++ index.php
@@ -1,25 +1,18 @@
 <?php
-require "message/Message.php";
-require "util/Mysql.php";
 interface Handler {
 	// 处理函数
 	function handle();
 }
+require "message/Message.php";
+require "util/Mysql.php";
+
+require "handler/behavior/UserBehavior.php";
 require "handler/login/Login.php";
 require "handler/login/Logout.php";
 require "handler/error/Error.php";
 require "handler/word/Title.php";
 require "handler/word/LoadingPage.php";
 require "handler/broadcast/Broadcast.php";
-require "handler/behavior/HelpWindow.php";
-require "handler/behavior/AboutWindow.php";
-require "handler/behavior/SettingWindow.php";
-require "handler/behavior/SuspensionWindow.php";
-require "handler/behavior/BehaviorMainWindow.php";
-require "handler/behavior/BehaviorAllWindow.php";
-require "handler/behavior/WebshotWindow.php";
-require "handler/behavior/Webshot.php";
-require "handler/behavior/BroadcastBehavior.php";
 // require "Resources/GetPicture.php";
 $bin = file_get_contents ( "php://input" );
 if ($bin == null || $bin == "") {
@@ -48,39 +41,7 @@ if ($bin == null || $bin == "") {
 			$handler->handle ();
 			break;
 		case 5000 :
-			$handler = new BehaviorAllWindow ( $bin );
-			$handler->handle ();
-			break;
-		case 5002 :
-			$handler = new BehaviorMainWindow ( $bin );
-			$handler->handle ();
-			break;
-		case 5004 :
-			$handler = new SuspensionWindow ( $bin );
-			$handler->handle ();
-			break;
-		case 5006 :
-			$handler = new SettingWindow ( $bin );
-			$handler->handle ();
-			break;
-		case 5008 :
-			$handler = new AboutWindow ( $bin );
-			$handler->handle ();
-			break;
-		case 5010 :
-			$handler = new HelpWindow ( $bin );
-			$handler->handle ();
-			break;
-		case 5012 :
-			$handler = new WebshotWindow ( $bin );
-			$handler->handle ();
-			break;
-		case 5014 :
-			$handler = new Webshot ( $bin );
-			$handler->handle ();
-			break;
-		case 5016 :
-			$handler = new BroadcastBehavior ( $bin );
+			$handler = new UserBehavior ( $bin );
 			$handler->handle ();
 			break;
 		case 6000 :
